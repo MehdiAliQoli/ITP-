db53b47ac5ff43c82549e34f2e613f3bc2c0306c
Mehdi Ali Qoli <162902639+MehdiAliQoli@users.noreply.github.com>
2024-11-24 11:30:28 +0500

    Create fibonacciUsingRecursion.cpp

---
+++ fibonacciUsingRecursion.cpp
@@ -0,0 +1,27 @@
+//fibonacchi Series using Recursion Leet Code 509
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int fb(int b){
+  
+  if(b==0||b==1){
+    return b;
+  }
+  
+  return fb(b-1) + fb(b-2);
+}
+
+
+int main() 
+{
+int s=7;
+cout<<fb(s);
+ 
+ 
+ 
+    return 0;
+}
